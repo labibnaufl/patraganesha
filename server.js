5b0214e61bd3d5c9b3bb75337894b837fd890ee2
labibnaufl <naufallabibnugroho@gmail.com>
2026-03-19 15:40:34 +0700

    fix: custom server for static file serving

---
+++ server.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const path = require('path')
+
+const app = next({ dev: false })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
